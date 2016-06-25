16b4c7ecd46fadb117bffdb50bc0c42d50b9a78c
David Corwin <dave@launchpadlab.com>
2016-06-25 09:25:27 -0500

    lodash, oninput, chart

--- app/views/project.js
+++ app/views/project.js
@@ -1,22 +1,6 @@
-import choo from 'choo'
+import choo  from 'choo'
+import _     from 'lodash'
 import Chart from 'chart.js'
-
-const chartData = (labels, data) => ({
-  labels: labels,
-  datasets: [{
-    data: data,
-    backgroundColor: [
-        "#FF6384",
-        "#36A2EB",
-        "#FFCE56"
-    ],
-    hoverBackgroundColor: [
-        "#FF6384",
-        "#36A2EB",
-        "#FFCE56"
-    ]
-  }]
-})
 
 const projectEpics = (state, project) => state.epic.epics.filter(epic => epic.project === project.id)
 const epicFeatures = (state, epic) => state.feature.features.filter(feature => feature.epic === epic.id)
@@ -27,18 +11,8 @@ export default (params, state, send) =>  {
   const epics   = projectEpics(state, project)
   const projectScore = epics.reduce((total, epic) => total + epicTotal(state, epic), 0)
 
-  // const foobar = (ctx) => {
-  //   const chart = new Chart(ctx.firstElementChild,{ type: 'pie', data: data })
-  //   console.log(ctx)
-  //   return ctx
-  // }
-
   const labels = epics.map(epic => epic.name)
   const data = epics.map(epic => epicTotal(state, epic))
-
-  setTimeout(() => {
-    new Chart(document.getElementById("foo"),{ type: 'pie', data: chartData(labels, data) })
-  }, 100)
 
   return choo.view`
     <div class="mui-row">
@@ -55,11 +29,14 @@ export default (params, state, send) =>  {
           <div class="epic-cards">
             ${epics.map(epic => epicCard(epic, state, send))}
           </div>
+          <button class="mui-btn mui-btn--fab mui-btn--primary mui--pull-right">+</button>
         </main>
       </div>
       <div class="mui-col-xs-12 mui-col-lg-6">
         <aside>
-          <canvas id="foo" width="400" height="400"></canvas>
+          <div style="width: 100%">
+            ${pieChart({name: 'foo', labels, data})}
+          </div>
         </aside>
       </div>
     </div>
@@ -86,6 +63,7 @@ const epicCard = (params, state, send) => {
           <div class="feature-cards">
             ${features.map(feature => featureCard(feature, state, send))}
           </div>
+          <button class="mui-btn mui-btn--flat mui-btn--primary mui--pull-right">Add</button>
         </div>
       </div>
     </div>
@@ -93,15 +71,53 @@ const epicCard = (params, state, send) => {
 }
 
 const featureCard = (params, state, send) => {
+
+  const _updateFeature = score =>
+    send('feature:update', { id: params.id, score: Number(score || 0) })
+
+  const updateFeature = _.debounce(_updateFeature, 300)
+
   return choo.view`
     <div class="mui-row">
       <div class="mui-col-xs-8">${params.name}</div>
       <div class="mui-col-xs-4">
         <input
           type="number"
-          onkeyup=${(e) => send('feature:update', { id: params.id, score: Number(e.currentTarget.value || 0) })}
+          oninput=${e => updateFeature(e.currentTarget.value)}
           value=${params.score}/>
       </div>
     </div>
   `
+}
+
+const pieChart = (params) => {
+
+  const chartData = (labels, data) => ({
+    labels: labels,
+    datasets: [{
+      data: data,
+      backgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56"
+      ],
+      hoverBackgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#FFCE56"
+      ]
+    }]
+  })
+
+  const chart = () => {
+    new Chart(
+      document.getElementById(params.name),
+      { type: 'pie', data: chartData(params.labels, params.data) }
+    )
+  }
+
+  // Hacky hack
+  setTimeout(chart, 50)
+
+  return choo.view`<canvas id=${params.name}></canvas>`
 }
